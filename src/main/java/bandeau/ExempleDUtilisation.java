69b659c8b71e04ba9f031ac1f60f32ff6b631a34
Rémi Bastide <Remi.Bastide@irit.fr>
2018-12-10 16:53:14 +0100

    Minor changes

--- src/main/java/bandeau/ExempleDUtilisation.java
+++ src/main/java/bandeau/ExempleDUtilisation.java
@@ -16,44 +16,43 @@ public class ExempleDUtilisation {
         Color fore = monBandeau.getForeground();
 
         monBandeau.setMessage("Hello");
-		monBandeau.sleep(1000);
-		monBandeau.setMessage("On va changer de police");
-		monBandeau.sleep(1000);
-		monBandeau.setMessage("Monospaced 15 Bold");
-		monBandeau.setFont(new Font("Monospaced", Font.BOLD, 15));
-		monBandeau.sleep(1000);
-		monBandeau.setMessage("SansSerif 15");
-		monBandeau.setFont(new Font("SansSerif", Font.PLAIN, 15));
-		monBandeau.sleep(1000);
-		monBandeau.setMessage("On va zoomer");
-		monBandeau.sleep(1000);
-		monBandeau.setMessage("Zoom........");
-		for (int i = 5; i < 60 ; i+=5) {
-			monBandeau.setFont(new Font("Dialog", Font.BOLD, 5+i));
-			monBandeau.sleep(100);
-		}
-		monBandeau.sleep(1000);
+	monBandeau.sleep(1000);
+	monBandeau.setMessage("On va changer de police");
+	monBandeau.sleep(1000);
+	monBandeau.setMessage("Monospaced 15 Bold");
+	monBandeau.setFont(new Font("Monospaced", Font.BOLD, 15));
+	monBandeau.sleep(1000);
+	monBandeau.setMessage("SansSerif 15");
+	monBandeau.setFont(new Font("SansSerif", Font.PLAIN, 15));
+	monBandeau.sleep(1000);
+	monBandeau.setMessage("On va zoomer");
+	monBandeau.sleep(1000);
+	monBandeau.setMessage("Zoom........");
+	for (int i = 5; i < 60 ; i+=5) {
+		monBandeau.setFont(new Font("Dialog", Font.BOLD, 5+i));
+		monBandeau.sleep(100);
+	}
+	monBandeau.sleep(1000);
 
-		monBandeau.setFont(new Font("Courier new", Font.PLAIN, 15));
-		monBandeau.setMessage("On va tourner");
-		monBandeau.sleep(1000);
-		monBandeau.setMessage("On tourne...");
-		for (int i = 0; i <= 100; i++) {
-			monBandeau.setRotation(2*Math.PI*i / 100);
-			monBandeau.sleep(100);
-		}
+	monBandeau.setFont(new Font("Courier new", Font.PLAIN, 15));
+	monBandeau.setMessage("On va tourner");
+	monBandeau.sleep(1000);
+	monBandeau.setMessage("On tourne...");
+	for (int i = 0; i <= 100; i++) {
+		monBandeau.setRotation(2*Math.PI*i / 100);
+		monBandeau.sleep(100);
+	}
 
-		monBandeau.setMessage("On va changer de couleur de fond");
-		monBandeau.sleep(1000);
-		monBandeau.setBackground(Color.DARK_GRAY);
-		monBandeau.setMessage("On va changer de couleur");
-		monBandeau.sleep(1000);
-		monBandeau.setForeground(Color.YELLOW);
-		monBandeau.sleep(1000);
-		monBandeau.setFont(font);
-		monBandeau.setForeground(fore);
-		monBandeau.setBackground(back);
-		monBandeau.setMessage("Termine");
-		//System.exit(0);
+	monBandeau.setMessage("On va changer de couleur de fond");
+	monBandeau.sleep(1000);
+	monBandeau.setBackground(Color.DARK_GRAY);
+	monBandeau.setMessage("On va changer de couleur");
+	monBandeau.sleep(1000);
+	monBandeau.setForeground(Color.YELLOW);
+	monBandeau.sleep(1000);
+	monBandeau.setFont(font);
+	monBandeau.setForeground(fore);
+	monBandeau.setBackground(back);
+	monBandeau.setMessage("Termine");
    }
 }
